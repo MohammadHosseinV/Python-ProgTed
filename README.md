19aa16112661abb05c04ca504468d9ba4d8eddca
Mohammad Hossein <habibpour.programming@gmail.com>
2023-11-18 23:51:54 -0800

    Part 3 of ProgTed

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Python-ProgTed"    git init   git add README.md   git commit -m "first commit"   git branch -M main   git remote add origin https://github.com/MohammadHosseinV/Python-ProgTed.git   git push -u origin main
---
+++ Variable.py
@@ -0,0 +1,57 @@
+# Variables
+# Youtube : ProgTed
+
+x = 1000
+y = 2000
+
+print(x) # 1000
+print(y) # 2000
+
+print('----------')
+
+x = y
+
+print(x) # 2000      
+print(y) # 2000
+
+print('----------')
+
+w , z = 100 , 200
+
+print(w) # 100
+print(z) # 200
+
+p = o = i = 'ProgTed'
+
+print(p) # ProgTed 
+print(o) # ProgTed 
+print(i) # ProgTed 
+
+print('----------')
+
+score = 100 
+
+print(score) # 100
+
+score = score - 10
+
+print(score) # 90
+
+age = 21
+
+print(2023 - age) # 2002
+
+# age12
+
+my_name = 'Mohammd' #Snak Case Variable naming
+myName = 'Hossein' # Camel Case Variable naming
+PI = 3.14
+MyName = 'Ali' # Upper Camel Case Variable naming
+
+print('---------------')
+
+age = 30
+Age = 20
+
+print(age) # 30
+print(Age) # 20
