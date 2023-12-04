69dbe3695c89254e67e3723a61b536210fd82c52
Mohammad Hossein Habibpour <147923097+MohammadHosseinV@users.noreply.github.com>
2023-12-04 09:15:36 -0800

    Numbers Youtube ProgTed

---
+++ 5 - Number.py
@@ -0,0 +1,71 @@
+# ProgTed
+
+# Mohammad Hossein Habibpour
+
+intNumber = 13
+
+floatNumber = 2.34
+
+complexNumber = 1 + 5j
+
+print(type(intNumber))
+print(type(floatNumber))
+print(type(complexNumber))
+
+print('-----------------------')
+
+# Operators
+
+x = 12
+
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) # Devision always return float
+print(type(x / y)) 
+
+print(2 ** 3) # power
+
+print(68 % 6) # باقی مانده
+print(68 / 6)
+print(68 // 6) # تقسیم عدد صحیح
+
+score = 100
+score = score - 10
+print(score)
+
+score = 100
+score -= 10
+print(score)
+
+x = 2465
+x /= 25
+print(x)
+
+age = 90
+age *= 3
+print(age)
+
+# convert data type
+
+# int(data)  | float(data)  | complex(data)
+
+result = 68 / 6
+print(result) # 11.333333333333334
+print(int(result)) # 11
+
+print(float(result))
+
+age = '22'
+
+print(type(age))
+
+print(int(age))
+print(type(int(age)))
+
+name = '52'
+print(float(name))
+
+print(complex(52))
