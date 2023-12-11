c7ddc776ec67944f5be49be97b4704fff23266ee
Mohammad Hossein Habibpour <147923097+MohammadHosseinV@users.noreply.github.com>
2023-12-11 13:31:18 -0800

    Kilometers to Miles

---
+++ 8 CalculateKMtoMiles.py
@@ -0,0 +1,35 @@
+# progTed
+
+# input() 
+# round() 2.9 => 3
+
+# age = input('Please enter your age : ') # input always return string
+# age = int(age)
+# print(2023 - age)
+
+# number = 3.4259847
+
+# print(round(number, 2)) # round(data:number , length float point) # 0.5 rounded to up
+
+
+print('----------------------------------------------------------')
+
+# name = input('Please enter your name :')
+
+# family = input('Please enter your family :')
+
+# print(name + ' ' + family)
+
+print('----------------------------------------------------------')
+
+# 1.609344
+
+Km = input('Please enter KM for convert to Miles :')
+
+Km = float(Km)
+
+miles = Km / 1.609344
+
+miles = round(miles , 3)
+
+print(f"{Km} km is {miles} miles")
