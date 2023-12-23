59207476762397ce6047672e29fa6ab8c7c0fa27
Mohammad Hossein Habibpour <147923097+MohammadHosseinV@users.noreply.github.com>
2023-12-23 02:01:07 -0800

    Conditions

---
+++ 10 Condition.py
@@ -0,0 +1,72 @@
+# ProgTed
+
+# name = input('Please enter your name :')
+
+# if name == 'Mohammad':
+#     print(f'Hello , {name}')
+
+# if expression :
+#     print(True)
+# else :
+#     print(False)
+
+# name = input('Please enter your name :')
+
+# if name == 'Mohammad':
+#     print(f'Hello , {name}')
+# elif name == 'nima':
+#     print(name)
+# else :
+#     print('Hello')
+
+# ------------------------------------
+
+# userAct = input('Do you want to continue?')
+
+# if userAct[0] == 'y':
+#     print('Continue ...')
+# elif userAct[0] == 'n':
+#     print('Stop')
+# else:
+#     print('This is Warring ...')
+
+# if in one line
+# print(True) if expression else print(False)
+# print('ProgTed') if 2 < 3 else print('Mohammad')
+
+number1 = 100
+if 2 < 3 :
+    print('ProgTed')
+    print('Nima')
+    number1 += 10
+    print(number1)
+else:
+    print('Mohammad')
+print('Nima')
+number1 += 10
+print(number1)
+
+# Even or Odd
+
+number = int(input('Please enter number : '))
+
+print(number % 2)
+
+if number % 2 == 0 :
+    print(f'{number} is Even')
+else:
+    print(f'{number} is Odd')
+
+
+'''
+practice
+
+number ----> user
+
+1st --> Gold
+2st --> Silver
+3st --> Bronze
+
+anouther player No Medal
+
+'''
