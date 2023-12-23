f43a28eba5c954324f794261c968bfa2c9b4c963
Mohammad Hossein Habibpour <147923097+MohammadHosseinV@users.noreply.github.com>
2023-12-23 02:02:35 -0800

    True & False

---
+++ 11 Project.py
@@ -0,0 +1,13 @@
+# progTed
+
+number = int(input('Please enter state : '))
+
+if number == 1 :
+    print('Oh my God , you get Gold medal')
+    print('Great')
+elif number == 2 :
+    print('you get silver medal')
+elif number == 3 :
+    print('you get Bronze medal')
+else:
+    print('You doesnt get any medal')
---
+++ 12 Truthiness & Falsiness.py
@@ -0,0 +1,59 @@
+# progted
+
+number = 50
+
+# if number == 50:
+#     print('True')
+
+# if number is 50:
+#     print('True')
+
+# Different between == and is
+
+# == : values are equal
+
+# is : point to the same object
+
+# if type(number) is int:
+#     print(f"{number} is int")
+# else :
+#     print('No')
+
+
+# myList1 = [1,2,3]
+
+# myList2 = myList1
+
+# myList3 = list(myList1)
+
+# print(myList1)
+# print(myList2)
+# print(myList3)
+
+# print(myList1 == myList2)
+# print(myList1 == myList3)
+
+# print(myList1 is myList2)
+# print(myList1 is myList3)
+
+
+# Falsiness Diffault
+
+# empty object : () [] {} , zero | 0 , empty string : '' "" , None
+
+# number = None
+
+# if number:
+#     print('Truthiness')
+# else:
+#     print('Falsiness')
+
+car = input('Please enter your favorite car :')
+
+if car:
+    print(car)
+else:
+    car = 'BMW'
+    print(f'your favorite car is {car}')
+
+
